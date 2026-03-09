dfb915f5564191c30f316795b4fe62aaa9325f4a
ittaqigadiza <ittaqigadiza@gmail.com>
2026-03-09 21:52:48 +0700

    Update eps1.js

--- eps1.js
+++ eps1.js
@@ -94,3 +94,28 @@ if(e.target.tagName === "IMG"){
 e.preventDefault();
 }
 });
+
+document.addEventListener("keydown", function(e) {
+
+if(e.ctrlKey && e.key === "u") {
+e.preventDefault();
+}
+
+if(e.ctrlKey && e.shiftKey && e.key === "I") {
+e.preventDefault();
+}
+
+if(e.ctrlKey && e.shiftKey && e.key === "J") {
+e.preventDefault();
+}
+
+if(e.key === "F12") {
+e.preventDefault();
+}
+
+});
+
+document.addEventListener("contextmenu", function(e){
+e.preventDefault();
+});
+
